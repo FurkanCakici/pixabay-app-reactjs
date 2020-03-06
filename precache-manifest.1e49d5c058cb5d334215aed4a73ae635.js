af52d34b57a2d2506adb4f840cd2a1fd6b7c393b
frknckc <furkancakici25@gmail.com>
2020-03-07 00:25:05 +0300

    Updates

---
+++ precache-manifest.1e49d5c058cb5d334215aed4a73ae635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e4f0b6f6f29f23ccc7c3af8a34c4dd",
+    "url": "/pixabay-api-app/index.html"
+  },
+  {
+    "revision": "1537cfa3c2ad94e4773d",
+    "url": "/pixabay-api-app/static/css/main.2b2bef18.chunk.css"
+  },
+  {
+    "revision": "7beafe42d270e5b51360",
+    "url": "/pixabay-api-app/static/js/2.18034f68.chunk.js"
+  },
+  {
+    "revision": "6d1ae0a7201d2aa71f08b67ad045a152",
+    "url": "/pixabay-api-app/static/js/2.18034f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1537cfa3c2ad94e4773d",
+    "url": "/pixabay-api-app/static/js/main.edc71bd9.chunk.js"
+  },
+  {
+    "revision": "c49f29d6314a93d196d0",
+    "url": "/pixabay-api-app/static/js/runtime-main.52566847.js"
+  }
+]);
